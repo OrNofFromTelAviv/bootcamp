2b7fa98b83a5f1d069d2d2b3c968c74b66f6ead2
OrNofFromTelAviv <iamornof@gmail.com>
2022-01-12 20:30:03 +0200

    first message

--- Week3/Day4/ExercisesXP.js
+++ Week3/Day4/ExercisesXP.js
@@ -1,18 +1,16 @@
-//Exercise 1
+//Exercise 1: Simple If/Else Statement
 let x = 10
 let y = 5
-
 if (x>y) {
     console.log("x is the biggest number")
 } else {
     console.log("y is the biggest number")
 }
 
-//Exercise 2
+//Exercise 2: Chihuahua
 let newDog = "Chihuahua";
 let length = newDog.length;
 console.log(length);
-
 console.log(newDog.toUpperCase());
 console.log(newDog.toLowerCase());
 
@@ -23,13 +21,35 @@ if (newDog = "Chihuahua") {
 }
 
 
-//Exercise 3
+//Exercise 3: Even Or Odd
 let number = prompt("What is your lucky number?")
-
 if (number % 2 == 0) {
 alert(number + " is an even number")
 } else
 alert(number + " is an odd number")
 
-//Exercise 4
+//Exercise 4: Group Chat
+let users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"]
+let length = users.length
 
+
+switch (length) {
+    case "0":
+        console.log("no one is online");
+        break;
+    case "1":
+        console.log(users[0]+ " is online")    
+        break;
+    case "2":
+        console.log(users.slice(0,2) + " are online")    
+        break;
+
+
+  
+}
+
+
+
+
+If there are more than 2 users, console.log the first two names in the users array and the number of additional users online.
+For example, if there are 5 users, it should display:
